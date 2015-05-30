a085504f2572d567a9d159294dc68c95c16968b2
zyang <yzwcroco@gmail.com>
2015-05-30 21:01:12 +0800

    优化下代码统计结果的输出

--- src/main/java/net/yangziwen/fileprocessor/executor/StatsExecutor.java
+++ src/main/java/net/yangziwen/fileprocessor/executor/StatsExecutor.java
@@ -1,9 +1,17 @@
 package net.yangziwen.fileprocessor.executor;
 
+import static org.apache.commons.io.FileUtils.ONE_EB;
+import static org.apache.commons.io.FileUtils.ONE_GB;
+import static org.apache.commons.io.FileUtils.ONE_KB;
+import static org.apache.commons.io.FileUtils.ONE_MB;
+import static org.apache.commons.io.FileUtils.ONE_PB;
+import static org.apache.commons.io.FileUtils.ONE_TB;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
@@ -16,6 +24,8 @@ import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 
 public class StatsExecutor implements Executor {
+	
+	private static final String COLUMN_SEPARATOR = " |";
 	
 	private FileStats overviewStats = new FileStats();
 	
@@ -54,45 +64,49 @@ public class StatsExecutor implements Executor {
 
 	@Override
 	public void after() {
-		int padSize = 12;
-		outputLine("[%d] files are investigated and the total size is [%s]", 
-				overviewStats.fileNum, 
-				FileUtils.byteCountToDisplaySize(overviewStats.fileSize));
-		outputLine("There are [%d] lines and [%d] nonblank lines", 
-				overviewStats.lineNum, overviewStats.nonblankLineNum);
-		outputLine("details are as follows:");
+		int padSize = 15;
+		outputLine();
 		outputLine(
-				StringUtils.rightPad("suffix", padSize)
+				StringUtils.leftPad("suffix" + COLUMN_SEPARATOR, padSize)
 				+
-				StringUtils.rightPad("files", padSize)
+				StringUtils.leftPad("files" + COLUMN_SEPARATOR, padSize)
 				+
-				StringUtils.rightPad("size", padSize)
+				StringUtils.leftPad("size" + COLUMN_SEPARATOR, padSize)
 				+
-				StringUtils.rightPad("lines", padSize)
+				StringUtils.leftPad("lines" + COLUMN_SEPARATOR, padSize)
 				+
-				StringUtils.rightPad("nonblanks", padSize)
+				StringUtils.leftPad("nonblanks" + COLUMN_SEPARATOR, padSize)
 		);
+		outputLine(StringUtils.repeat(StringUtils.repeat("-", padSize - 1) + "+", 5));
+		outputSuffixStats("[all]", overviewStats, padSize);
 		for(Entry<String, FileStats> entry: suffixStatsMap.entrySet()) {
-			FileStats suffixStats = entry.getValue();
-			outputLine(
-					StringUtils.rightPad(entry.getKey(), padSize)
-					+
-					StringUtils.rightPad(String.valueOf(suffixStats.fileNum), padSize)
-					+
-					StringUtils.rightPad(FileUtils.byteCountToDisplaySize(suffixStats.fileSize), padSize)
-					+
-					StringUtils.rightPad(String.valueOf(suffixStats.lineNum), padSize)
-					+
-					StringUtils.rightPad(String.valueOf(suffixStats.nonblankLineNum), padSize)
-			);
+			outputSuffixStats(entry.getKey(), entry.getValue(), padSize);
 		}
 	}
 	
-	private void outputLine(String format, Object... args) {
+	private static void outputSuffixStats(String suffix, FileStats suffixStats, int padSize) {
+		outputLine(
+				StringUtils.leftPad(suffix + COLUMN_SEPARATOR, padSize)
+				+
+				StringUtils.leftPad(String.valueOf(suffixStats.fileNum) + COLUMN_SEPARATOR, padSize)
+				+
+				StringUtils.leftPad(byteCountToDisplaySize(suffixStats.fileSize) + COLUMN_SEPARATOR, padSize)
+				+
+				StringUtils.leftPad(String.valueOf(suffixStats.lineNum) + COLUMN_SEPARATOR, padSize)
+				+
+				StringUtils.leftPad(String.valueOf(suffixStats.nonblankLineNum) + COLUMN_SEPARATOR, padSize)
+		);
+	}
+	
+	private static void outputLine() {
+		outputLine("");
+	}
+	
+	private static void outputLine(String format, Object... args) {
 		System.out.println(String.format(format, args));
 	}
 	
-	private FileStats ensureStats(String key, Map<String, FileStats> statsMap) {
+	private static FileStats ensureStats(String key, Map<String, FileStats> statsMap) {
 		if(!statsMap.containsKey(key)) {
 			statsMap.put(key, new FileStats());
 		}
@@ -105,6 +119,38 @@ public class StatsExecutor implements Executor {
 		int fileSize;
 		int lineNum;
 		int nonblankLineNum;
+	}
+	
+	private static String byteCountToDisplaySize(int byteCount) {
+		return byteCountToDisplaySize(byteCount, 2);
+	}
+	
+	private static String byteCountToDisplaySize(int byteCount, int digitsAfterDecimalPoint) {
+		double doubleSize = byteCount;
+		String unit = " b";
+		if (byteCount / ONE_EB > 0) {
+			doubleSize /= ONE_EB;
+            unit = "EB";
+        } else if (byteCount / ONE_PB > 0) {
+        	doubleSize /= ONE_PB;
+            unit = "PB";
+        } else if (byteCount / ONE_TB > 0) {
+            doubleSize /= ONE_TB;
+            unit = "TB";
+        } else if (byteCount / ONE_GB > 0) {
+        	doubleSize /= ONE_GB;
+        	unit = "GB";
+        } else if (byteCount / ONE_MB > 0) {
+        	doubleSize /= ONE_MB;
+        	unit = "MB";
+        } else if (byteCount / ONE_KB > 0) {
+        	doubleSize /= ONE_KB;
+        	unit = "KB";
+        }
+		DecimalFormat format = Math.floor(doubleSize) < doubleSize && digitsAfterDecimalPoint > 0
+				? new DecimalFormat("." + StringUtils.repeat('0', digitsAfterDecimalPoint))
+				: new DecimalFormat("#");
+        return format.format(doubleSize) + " " + unit;
 	}
 
 }
